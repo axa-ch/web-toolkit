1051e3b7b8c2172b947f2958735540f9ef1670a8
David Knezić <davidknezic@gmail.com>
2014-08-26 21:16:07 +0200

    Extract Metalsmith task from gulpfile

---
+++ gulp/docs.js
@@ -0,0 +1,48 @@
+/**
+ * @file The Metalsmith workflow that builds our docs.
+ */
+
+module.exports = exports = function (options) {
+  var path = require('path');
+  var options = options || {};
+
+  options.cwd = options.cwd || './';
+  options.src = options.src || './src';
+  options.dest = options.dest || './dest';
+
+  return function (cb) {
+    var Metalsmith = require('metalsmith');
+    var collections = require('metalsmith-collections');
+    var templates = require('metalsmith-templates');
+    var jade = require('metalsmith-jade');
+    var ignore = require('metalsmith-ignore');
+    var branch = require('metalsmith-branch');
+
+    var metalsmith = new Metalsmith(options.cwd);
+    metalsmith.source(options.src);
+
+    // put the visuals to metadata
+    metalsmith.use(branch('visuals/*.jade')
+        .use(jade({ locals: metalsmith.metadata() }))
+        .use(collections({ visuals: 'visuals/*' }))
+        .use(ignore('visuals/*')));
+
+    // do the static pages
+    metalsmith.use(branch('*.jade')
+        .use(jade({ locals: metalsmith.metadata() }))
+        .use(templates({
+            engine: 'jade',
+            directory: path.join(options.src, 'layouts')
+        })));
+
+    // we no need the layouts
+    metalsmith.use(ignore('layouts/*'));
+
+    metalsmith.destination(options.dest);
+
+    return metalsmith.build(function (err) {
+      if (err) return cb(err);
+      cb();
+    });
+  };
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,15 @@
 var gulp = require('gulp');
+var bg = require('gulp-bg');
+var docs = require('./gulp/docs');
 
-gulp.task('docs', function (cb) {
-  var Metalsmith = require('metalsmith'),
-      collections = require('metalsmith-collections'),
-      templates = require('metalsmith-templates'),
-      jade = require('metalsmith-jade'),
-      ignore = require('metalsmith-ignore'),
-      branch = require('metalsmith-branch');
+gulp.task('docs', docs({
+  cwd: __dirname,
+  src: './src/docs',
+  dest: './dist/docs'
+}));
 
-  var metalsmith = new Metalsmith(__dirname);
-  metalsmith.source('./src/docs');
-
-  // put the visuals to metadata
-  metalsmith.use(branch('visuals/*.jade')
-      .use(jade({ locals: metalsmith.metadata() }))
-      .use(collections({ visuals: 'visuals/*' }))
-      .use(ignore('visuals/*')));
-
-  // do the static pages
-  metalsmith.use(branch('*.jade')
-      .use(jade({ locals: metalsmith.metadata() }))
-      .use(templates({
-          engine: 'jade',
-          directory: './src/docs/layouts'
-      })));
-
-  // we no need the layouts
-  metalsmith.use(ignore('layouts/*'));
-
-  metalsmith.destination('./dist/docs');
-
-  return metalsmith.build(function (err) {
-    if (err) return cb(err);
-    var m = metalsmith.metadata();
-    cb();
-  });
+gulp.task('watch-docs', function (cb) {
+  gulp.watch('./src/docs/**', ['docs']);
 });
 
 gulp.task('default', []);
