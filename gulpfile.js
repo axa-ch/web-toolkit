94dad88e5ef65e51cc4238dbc51884b0a083d70c
David Knezić <davidknezic@gmail.com>
2014-08-31 22:41:38 +0200

    Introduce SVG icons and web font generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var iconfont = require('gulp-iconfont');
 var docs = require('./tasks/docs');
 
 gulp.task('docs', docs({
@@ -9,6 +10,18 @@ gulp.task('docs', docs({
 
 gulp.task('watch-docs', function (cb) {
   gulp.watch('./docs/**', ['docs']);
+});
+
+gulp.task('icons', function (cb) {
+  gulp.src(['icons/*.svg'])
+    .pipe(iconfont({
+      fontName: 'style-guide-font',
+      appendCodepoints: true
+    }))
+    .on('codepoints', function(codepoints) {
+      console.log(codepoints);
+    })
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('default', []);
