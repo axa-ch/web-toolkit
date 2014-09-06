25a88cc6ce027bf94fa729a8b3aadc2c6197a28e
David Knezić <davidknezic@gmail.com>
2014-09-06 17:20:08 +0200

    Add gulp tasks to serve & livereload docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
 
 var docs = require('./tasks/docs');
 
@@ -16,8 +17,25 @@ gulp.task('watch-docs', function (cb) {
   gulp.watch('./docs/**', ['docs']);
 });
 
+gulp.task('serve-docs', function (cb) {
+  var connect = require('connect');
+  var serveStatic = require('serve-static');
+
+  connect()
+    .use(serveStatic('./dist/docs'))
+    .listen(process.env.PORT || 3000, cb);
+});
+
+gulp.task('develop-docs', ['serve-docs'], function (cb) {
+  var server = livereload();
+
+  gulp.watch('./dist/docs/**').on('change', function (file) {
+    server.changed(file.path);
+  });
+});
+
 gulp.task('icons', function (cb) {
-  gulp.src(['icons/*.svg'])
+  gulp.src(['./icons/*.svg'])
     .pipe(iconfont({
       fontName: 'style-guide-font',
       appendCodepoints: true
@@ -25,18 +43,18 @@ gulp.task('icons', function (cb) {
     .on('codepoints', function(codepoints) {
       console.log(codepoints);
     })
-    .pipe(gulp.dest('dist/fonts'));
+    .pipe(gulp.dest('./dist/fonts'));
 });
 
 gulp.task('less', function (cb) {
-  gulp.src(['less/style.less', 'less/normalize.less'])
+  gulp.src(['./less/style.less', './less/normalize.less'])
     .pipe(sourcemaps.init())
     .pipe(less({
       paths: ['less']
     }))
     .on('error', cb)
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('default', []);
