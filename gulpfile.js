d417f384d3aa2c994d73bb87159cd11ee24f8725
David Knezić <davidknezic@gmail.com>
2014-09-14 15:19:15 +0200

    Simplify gulp file, stabilize live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp');
+// FIXME: Merge docs & assets building tasks as much as possible!
 
+var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
@@ -7,8 +8,23 @@ var livereload = require('gulp-livereload');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var runSequence = require('run-sequence');
+var watch = require('gulp-watch');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var gutil = require('gulp-util');
+var clean = require('gulp-clean');
 
 var docs = require('./tasks/docs');
+
+function errorify(e) {
+  gutil.beep();
+  gutil.log(e);
+}
+
+gulp.task('clean', function () {
+  return gulp.src('./dist', { read: false })
+    .pipe(clean());
+});
 
 gulp.task('docs', docs({
   cwd: __dirname,
@@ -16,74 +32,67 @@ gulp.task('docs', docs({
   dest: './dist/docs'
 }));
 
-gulp.task('watch-docs', function (cb) {
-  gulp.watch('./docs/**', ['docs']);
-});
-
-gulp.task('serve-docs', function (cb) {
-  var connect = require('connect');
-  var serveStatic = require('serve-static');
-
-  connect()
-    .use(serveStatic('./dist/docs'))
-    .listen(process.env.PORT || 3000);
-});
-
-gulp.task('reload-docs', function (cb) {
-  var server = livereload();
-
-  gulp.watch('./dist/docs/**').on('change', function (file) {
-    server.changed(file.path);
-  });
-});
-
-gulp.task('develop-docs', function (cb) {
-  runSequence([
-    'watch-docs',
-    'serve-docs',
-    'reload-docs'
-  ], cb);
-});
-
-gulp.task('icons', function (cb) {
-  gulp.src(['./icons/*.svg'])
+gulp.task('icons', function () {
+  return gulp.src(['./icons/*.svg'])
     .pipe(iconfont({
       fontName: 'style-guide-font',
       appendCodepoints: true
     }))
-    .on('codepoints', function(codepoints) {
-      console.log(codepoints);
+    .on('error', errorify)
+    .on('codepoints', function (codepoints) {
+      // FIXME: Create less-file with codepoints here
     })
     .pipe(gulp.dest('./dist/fonts'));
 });
 
-gulp.task('less', function (cb) {
-  gulp.src(['./less/style.less', './less/normalize.less'])
+gulp.task('styles', function () {
+  return gulp.src(['./less/style.less', './less/normalize.less'])
     .pipe(sourcemaps.init())
     .pipe(less({
       paths: ['less']
     }))
-    .on('error', cb)
+    .on('error', errorify)
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('coffee', function (cb) {
-  gulp.src('./coffee/*.coffee')
+gulp.task('scripts', function () {
+  return gulp.src('./coffee/*.coffee')
     .pipe(sourcemaps.init())
     .pipe(coffee())
+    .on('error', errorify)
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('combine-coffee', ['coffee'], function (cb) {
-  gulp.src('./dist/js/*.js')
-    .pipe(sourcemaps.init({
-      loadMaps: true
-    }))
+    .pipe(gulp.dest('./dist/js'))
     .pipe(concat('style.all.js'))
+    .on('error', errorify)
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('default', []);
+gulp.task('build', function (cb) {
+  runSequence(['docs', 'icons', 'scripts', 'styles'], cb);
+});
+
+gulp.task('serve', function (next) {
+  connect()
+    .use(serveStatic('./dist/docs'))
+    .listen(process.env.PORT || 3000, next);
+});
+
+gulp.task('dev', ['docs', 'serve'], function () {
+  livereload.listen();
+
+  watch([
+    './docs/**',
+    './less/**',
+    './icons/**',
+    './coffee/**'
+  ], function (files, callback) {
+    runSequence('docs', function (arguments) {
+      livereload.changed();
+      callback.apply(this, arguments);
+    });
+  });
+});
+
+gulp.task('default', ['build']);
