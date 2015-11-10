9b8d7a0f543ca234b12745cad4d86fe47396af62
David Knezić <davidknezic@gmail.com>
2015-11-10 21:11:55 +0100

    Move notification example code to index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,5 @@
+import $ from 'jquery'
+
 require('../..')
 require('./cheat')
 require('./example')
@@ -10,3 +12,15 @@ require('./tab')
 import moment from 'moment'
 
 moment.locale('en_GB')
+
+$('#showNotification').on('click', function () {
+  let content = $('#notification_content').val()
+  let modifier = $('[name="notification__modifier"]:checked').val()
+  let isHtml = $('#notification_html').is(':checked')
+
+  $('#notification').notification({
+    content: content,
+    modifier: modifier,
+    html: isHtml
+  })
+})
