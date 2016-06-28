91f9895e576e72a2a81e9a8363be977088dceb9c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-06-28 17:41:07 +0200

    fixed ESLINT errors

--- jquery/summary.js
+++ jquery/summary.js
@@ -14,26 +14,25 @@ class Summary {
 
   init() {
 
-    let firstY = this.$element.find('h3').first().offset().top - 14;
-    let lastY = this.$element.find('h3').last().offset().top - 14;
+    const firstY = this.$element.find('h3').first().offset().top - 14;
+    const lastY = this.$element.find('h3').last().offset().top - 14;
 
-    this.$element.append('<style>.summary--magic:before{ top: ' + firstY + 'px; height: ' + (lastY-firstY) + 'px; }</style>');
+    this.$element.append(`<style>.summary--magic:before{ top: ${firstY}px; height: ${(lastY-firstY)}px; }</style>`);
 
-    /*
+    /*`
     <svg class="icons-list__icon icon">
       <use xlink:href="/images/icons.svg#add"></use>
     </svg>
     */
-
   }
 
 }
 
 function Plugin() {
-  let params = arguments
+  const params = arguments
 
   return this.each(function () {
-    let $this = $(this)
+    const $this = $(this)
     let data = $this.data('axa.summary')
 
     if (!data) {
@@ -53,8 +52,8 @@ $.fn.summary.Constructor = Summary
 
 $(function () {
   $('[data-summary]').each(function () {
-    let $summary = $(this)
-    let data = $summary.data()
+    const $summary = $(this)
+    const data = $summary.data()
     Plugin.call($summary, data)
   })
 })
