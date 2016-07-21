940f3ea14c6d62f461bdcab3ae6ed9756aa6af1c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:52:46 +0200

    fixed most eslint erros of docs iframe-resizer

--- docs/js/iframe-resizer.js
+++ docs/js/iframe-resizer.js
@@ -1,9 +1,9 @@
-import $ from 'jquery';
-import iFrameResize from 'iframe-resizer';
+import $ from 'jquery'
+import iFrameResize from 'iframe-resizer'
 
 class IFrameResizer {
   constructor(el, options) {
-    this.$el = $(el);
+    this.$el = $(el)
 
     iFrameResize({
       autoResize: true,
@@ -12,30 +12,32 @@ class IFrameResizer {
       sizeHeight: true,
       maxHeight: options.maxHeight || 'infinity',
       minHeight: options.minHeight || 0,
-      heightCalculationMethod: 'lowestElement'
-    }, this.$el[0]);
+      heightCalculationMethod: 'lowestElement',
+    }, this.$el[0])
   }
 }
 
-let Plugin = function(option) {
-  let $el = $(this);
-  let data = $el.data('axa.iframe-resizer');
-  let options = typeof option === 'object' && option;
+const Plugin = (option) => {
+  const $el = $(this)
+  const data = $el.data('axa.iframe-resizer')
+  const options = typeof option === 'object' && option
 
   // there's no instance yet
   if (!data) {
-    data = new IFrameResizer(this, options);
-    return $el.data('axa.iframe-resizer', data);
+    data = new IFrameResizer(this, options)
+    return $el.data('axa.iframe-resizer', data)
   }
-};
+
+  return data
+}
 
 $(() =>
-  $('[data-iframe-resizer]').each(function() {
-    let $el = $(this);
-    let data = $el.data();
+  $('[data-iframe-resizer]').each(() => {
+    const $el = $(this)
+    const data = $el.data()
 
-    return Plugin.call($el, data);
+    return Plugin.call($el, data)
   })
-);
+)
 
 //! Copyright AXA Versicherungen AG 2015
