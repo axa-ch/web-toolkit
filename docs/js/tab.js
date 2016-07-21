3d20656cf52f9ffb110320a5ea5a4438cbd7c72c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:53:38 +0200

    fixed most eslint erros of docs tabs

--- docs/js/tab.js
+++ docs/js/tab.js
@@ -1,58 +1,58 @@
-import $ from 'jquery';
+/* global window */
+
+import $ from 'jquery'
 
 // Public class definition
 class Tab {
-
   constructor(element, options) {
-    this.$element = $(element);
+    this.$element = $(element)
     // @options = $.extend {}, options
 
-    this.$toggles = this.$element.find('[data-tab-toggle]');
+    this.$toggles = this.$element.find('[data-tab-toggle]')
     this.$toggles.on('click', this, event => event.data.toggle(event)
-    );
+    )
 
-    let defaultSelector = this.$element.data('tab-default');
-    this.setActiveTab(defaultSelector);
+    const defaultSelector = this.$element.data('tab-default')
+    this.setActiveTab(defaultSelector)
   }
 
   toggle(event) {
-    let $toggle = $(event.target);
-    let selector = $toggle.data('tab-toggle');
-    return this.setActiveTab(selector);
+    const $toggle = $(event.target)
+    const selector = $toggle.data('tab-toggle')
+    this.setActiveTab(selector)
   }
 
   setActiveTab(tabName) {
     if (this.activeTabName === tabName) {
-      return;
+      return
     }
-    this.activeTabName = tabName;
+
+    this.activeTabName = tabName
+
     if (this.$activeContentPanel != null) {
-//        @$activeContentPanel.toggleClass 'is-active'
-      this.$activeContentPanel.slideToggle('fast', function() {
-        return $(this).toggleClass('is-active');
-      }
-      );
+      // @$activeContentPanel.toggleClass 'is-active'
+      this.$activeContentPanel.slideToggle('fast', () => {
+        $(this).toggleClass('is-active')
+      })
     }
     if (this.$activeToggle != null) {
-      this.$activeToggle.toggleClass('is-active');
+      this.$activeToggle.toggleClass('is-active')
     }
 
-    this.$activeContentPanel = this.$element.find(`[data-tab-content='${tabName}']`);
-    this.$activeToggle = this.$element.find(`[data-tab-toggle='${tabName}']`);
+    this.$activeContentPanel = this.$element.find(`[data-tab-content='${tabName}']`)
+    this.$activeToggle = this.$element.find(`[data-tab-toggle='${tabName}']`)
 
-//      @$activeContentPanel.toggleClass 'is-active'
-    this.$activeContentPanel.slideToggle('fast', function() {
-      return $(this).toggleClass('is-active');
-    }
-    );
-    return this.$activeToggle.toggleClass('is-active');
+    // @$activeContentPanel.toggleClass 'is-active'
+    this.$activeContentPanel.slideToggle('fast', () => {
+      $(this).toggleClass('is-active')
+    })
+    this.$activeToggle.toggleClass('is-active')
   }
 }
 
 // DATA-API
 $(window).on('load', () =>
   $('[data-tab]').each((index, element) => new Tab(element))
-
-);
+)
 
 //! Copyright AXA Versicherungen AG 2015
