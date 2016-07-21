c4253f2bc11246497266035c176aa3c18127147a
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:26:27 +0200

    fixed most eslint erros of gulp styles-variables task

--- tasks/styles-variables.js
+++ tasks/styles-variables.js
@@ -1,15 +1,14 @@
-import gulp from 'gulp';
-import template from 'gulp-template';
-import rename from 'gulp-rename';
+import gulp from 'gulp'
+import template from 'gulp-template'
+import rename from 'gulp-rename'
 
-import readJSONFile from '../lib/readJSONFile';
+import readJSONFile from '../lib/readJSONFile'
 
 export default () =>
 
-  gulp.src([ './less/style/variables.less.lodash' ], { base: './less/' })
+  gulp.src(['./less/style/variables.less.lodash'], { base: './less/' })
     .pipe(template({ colors: readJSONFile('./less/colors.json') }))
     .pipe(rename({ extname: '' }))
     .pipe(gulp.dest('./dist/less'))
-;
 
 //! Copyright AXA Versicherungen AG 2015
