47cd8715b480f499f6d3962b8e74eec3341fcbf2
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:22:09 +0200

    fixed most eslint erros of gulp dev task

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,27 +1,27 @@
-import gulp from 'gulp';
-import watch from 'gulp-watch';
-import sequence from 'gulp-watch-sequence';
-import webserver from 'gulp-webserver';
+import gulp from 'gulp'
+import watch from 'gulp-watch'
+import sequence from 'gulp-watch-sequence'
+import webserver from 'gulp-webserver'
 
-import errorify from '../lib/errorify';
+import errorify from '../lib/errorify'
 
-export default [['build'], function(cb) {
+export default [['build'], function (cb) {
   gulp.src(['dist/docs'])
     .pipe(webserver({
       host: '0.0.0.0',
       port: process.env.PORT || 3000,
       livereload: {
         enable: true,
-        port: 35730
+        port: 35730,
       },
       middleware(req, res, next) {
         // make /global.html accessible on /global
         if (req.url && req.url === '/global') {
-          req.url = '/global.html';
+          req.url = '/global.html'
         }
-        return next();
-      }
-    }));
+        return next()
+      },
+    }))
 
   watch([
     './docs/page/**/*',
@@ -29,69 +29,60 @@ export default [['build'], function(cb) {
     './tmp/icons.json',
     './less/colors.json',
     './package.json',
-    './docs/config.json'
-  ], (files, cb) => gulp.start('docs-pages', cb)
-  )
-  .on('error', err => errorify(err.message, 'docs-pages'));
+    './docs/config.json',
+  ], (files, cb) => gulp.start('docs-pages', cb))
+  .on('error', err => errorify(err.message, 'docs-pages'))
 
   watch([
-    './modernizr.json'
-  ], function() {
-    gulp.start('modernizr', 'docs-scripts', 'docs-example-scripts', 'docs-inspiration-scripts');
-  }
-  )
-  .on('error', err => errorify(err.message, 'modernizr'));
+    './modernizr.json',
+  ], function () {
+    gulp.start('modernizr', 'docs-scripts', 'docs-example-scripts', 'docs-inspiration-scripts')
+  })
+  .on('error', err => errorify(err.message, 'modernizr'))
 
   watch([
-    './docs/js/**'
-  ], function() {
-    gulp.start('docs-scripts');
-  }
-  )
-  .on('error', err => errorify(err.message, 'docs-scripts'));
+    './docs/js/**',
+  ], function () {
+    gulp.start('docs-scripts')
+  })
+  .on('error', err => errorify(err.message, 'docs-scripts'))
 
   watch([
     './docs/less/**/*.less',
-    './dist/css/style.css'
-  ], () => gulp.start('docs-styles')
-  )
-  .on('error', err => errorify(err.message, 'docs-styles'));
+    './dist/css/style.css',
+  ], () => gulp.start('docs-styles'))
+  .on('error', err => errorify(err.message, 'docs-styles'))
 
   watch([
     './modernizr.json',
     './docs/images/**/*',
     './dist/images/**/*',
     './dist/fonts/**/*',
-    './dist/css/{style,normalize}.min.css{,.map}'
-  ], () => gulp.start('docs-assets')
-  )
-  .on('error', err => errorify(err.message, 'docs-assets'));
+    './dist/css/{style,normalize}.min.css{,.map}',
+  ], () => gulp.start('docs-assets'))
+  .on('error', err => errorify(err.message, 'docs-assets'))
 
   watch([
-    'icons/**/*.svg'
-  ], () => gulp.start('icons-svg', 'icons')
-  )
-  .on('error', err => errorify(err.message, 'icons-svg'));
+    'icons/**/*.svg',
+  ], () => gulp.start('icons-svg', 'icons'))
+  .on('error', err => errorify(err.message, 'icons-svg'))
 
   watch([
-    './images/**/*'
-  ], () => gulp.start('images')
-  )
-  .on('error', err => errorify(err.message, 'images'));
+    './images/**/*',
+  ], () => gulp.start('images'))
+  .on('error', err => errorify(err.message, 'images'))
 
   watch([
-    './jquery/**/*'
+    './jquery/**/*',
   ], (sequence(300)).getHandler('scripts', 'docs-scripts'))
-  .on('error', err => errorify(err.message, 'scripts'));
+  .on('error', err => errorify(err.message, 'scripts'))
 
   return watch([
     './less/**/*.less',
     './icons/*.svg',
-    './less/colors.json'
-  ], () => gulp.start('styles')
-  )
-  .on('error', err => errorify(err.message, 'styles'));
-}
-];
+    './less/colors.json',
+  ], () => gulp.start('styles'))
+  .on('error', err => errorify(err.message, 'styles'))
+}]
 
 //! Copyright AXA Versicherungen AG 2015
