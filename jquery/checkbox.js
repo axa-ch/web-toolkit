ae42eceea7ec509635cd35bc095471afb4ee131c
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 20:38:48 +0200

    fixed most eslint erros of jquery checkbox

--- jquery/checkbox.js
+++ jquery/checkbox.js
@@ -1,47 +1,55 @@
-import $ from 'jquery';
+/* global window */
+
+import $ from 'jquery'
 
 // Public class definition
 class Checkbox {
   static DEFAULTS
 
   constructor(element, options) {
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-    this.setCheckboxState = this.setCheckboxState.bind(this);
-    this.$element = $(element);
+    this.handleKeyUp = this.handleKeyUp.bind(this)
+    this.setCheckboxState = this.setCheckboxState.bind(this)
+    this.$element = $(element)
 
     // TODO: Do not depend on css classes
-    this.$checkbox = this.$element.find('.checkbox__checkbox');
-    this.$label = this.$element.find('.checkbox__label');
+    this.$checkbox = this.$element.find('.checkbox__checkbox')
+    this.$label = this.$element.find('.checkbox__label')
 
-    this.options = $.extend({}, Checkbox.DEFAULTS, options);
+    this.options = $.extend({}, Checkbox.DEFAULTS, options)
 
-    this.init();
+    this.init()
   }
 
   init() {
-    this.$checkbox.attr('tabindex', '-1');
-    this.$label.attr('tabindex', '0');
+    this.$checkbox.attr('tabindex', '-1')
+    this.$label.attr('tabindex', '0')
 
-    this.$element.addClass('checkbox--js');
+    this.$element.addClass('checkbox--js')
 
-    this.setCheckboxState();
+    this.setCheckboxState()
 
-    this.$checkbox.on('change', this.setCheckboxState);
+    this.$checkbox.on('change', this.setCheckboxState)
 
-    this.$label.on('keyup', this.handleKeyUp);
+    this.$label.on('keyup', this.handleKeyUp)
 
     if (typeof this.options.color !== 'undefined') {
-      this.handleCustomColorBG();
-      this.$element.css({'border-color': this.options.color});
-      return this.$element.on('click', this.handleCustomColorBG.bind(this));
+      this.handleCustomColorBG()
+      this.$element.css({ 'border-color': this.options.color })
+      this.$element.on('click', this.handleCustomColorBG.bind(this))
     }
   }
 
   handleCustomColorBG() {
     if (this.$checkbox.is(':checked')) {
-      return this.$element.css({'background-color': this.options.color, 'color': '#fff'});
+      this.$element.css({
+        'background-color': this.options.color,
+        color: '#fff',
+      })
     } else {
-      return this.$element.css({'background-color': '#fff', 'color': '#000'});
+      this.$element.css({
+        'background-color': '#fff',
+        color: '#000',
+      })
     }
   }
 
@@ -49,54 +57,51 @@ class Checkbox {
   handleKeyUp(e) {
     if (e.which === 32) {
       // prevent scrolling
-      e.preventDefault();
+      e.preventDefault()
 
-      this.$checkbox.prop('checked', !(this.$checkbox.is(':checked')));
+      this.$checkbox.prop('checked', !(this.$checkbox.is(':checked')))
 
       // Emit a change event manually
-      return this.$checkbox.change();
+      this.$checkbox.change()
     }
   }
 
   // Updates the UI according to the checkbox state
   setCheckboxState() {
     if (this.$checkbox.is(':checked')) {
-      return this.$element.addClass('is-active');
+      this.$element.addClass('is-active')
     } else {
-      return this.$element.removeClass('is-active');
+      this.$element.removeClass('is-active')
     }
   }
 }
 
 // Plugin definition
-let Plugin = function(option) {
-  let params = arguments;
+const Plugin = (option) => this.each(() => {
+  const $this = $(this)
+  let data = $this.data('axa.checkbox')
+  const options = $.extend({}, Checkbox.DEFAULTS, data, typeof option === 'object' && option)
 
-  return this.each(function() {
-    let $this = $(this);
-    let options = $.extend({}, Checkbox.DEFAULTS, data, typeof option === 'object' && option);
-    var data = $this.data('axa.checkbox');
+  if (!data) {
+    data = new Checkbox(this, options)
+    return $this.data('axa.checkbox', data)
+  }
 
-    if (!data) {
-      data = new Checkbox(this, options);
-      return $this.data('axa.checkbox', data);
-    }
-  });
-};
+  return data
+})
 
 // Plugin registration
-$.fn.checkbox = Plugin;
-$.fn.checkbox.Constructor = Checkbox;
+$.fn.checkbox = Plugin
+$.fn.checkbox.Constructor = Checkbox
 
 // DATA-API
 $(window).on('load', () =>
-  $('[data-checkbox]').each(function() {
-    let $checkbox = $(this);
-    let data = $checkbox.data();
+  $('[data-checkbox]').each(() => {
+    const $checkbox = $(this)
+    const data = $checkbox.data()
 
-    return Plugin.call($checkbox, data);
+    Plugin.call($checkbox, data)
   })
-
-);
+)
 
 //! Copyright AXA Versicherungen AG 2015
