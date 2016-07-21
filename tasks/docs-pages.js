fbd699cd046e2d16bb8e9f581c398be91adcbb86
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 14:36:14 +0200

    decaffeinated docs-pages.coffee task

---
+++ tasks/docs-pages.js
@@ -0,0 +1,205 @@
+import path from 'path';
+import marked from 'marked';
+import colors from 'colors';
+import gutil from 'gulp-util';
+import moment from 'moment';
+import _ from 'lodash';
+import fs from 'fs';
+
+import readJSONFile from '../lib/readJSONFile';
+import sampleJadeFilter from '../lib/jade-filter-sample';
+import highlightCodeJadeFilter from '../lib/jade-filter-highlightcode';
+import ignoreFmJadeFilter from '../lib/jade-filter-ignorefrontmatter';
+import markedRenderer from '../lib/marked-renderer';
+import markedChangelogRenderer from '../lib/marked-renderer-changelog';
+import searchIndexData from '../lib/search-index-data';
+import loadChangelog from '../lib/load-changelog';
+import navigation from '../lib/metalsmith-navigation';
+import relate from '../lib/metalsmith-relate';
+
+import Metalsmith from 'metalsmith';
+import collections from 'metalsmith-collections';
+import templates from 'metalsmith-templates';
+import jade from 'metalsmith-jade';
+import markdown from 'metalsmith-markdown';
+import branch from 'metalsmith-branch';
+import define from 'metalsmith-define';
+import drafts from 'metalsmith-drafts';
+import filepath from 'metalsmith-filepath';
+import relative from 'metalsmith-relative';
+import lunr from 'metalsmith-lunr';
+import copy from 'metalsmith-copy';
+import ignore from 'metalsmith-ignore';
+
+export default function(cb) {
+  let cwd = path.join(__dirname, '../');
+  let config = readJSONFile(path.join(cwd, './docs/config.json'));
+  let metalsmithSource = './docs/page';
+
+  // basedir for include paths
+  let jadeBaseDir = path.join(cwd, metalsmithSource);
+
+  // Jade filters
+  jade.registerFilter('sample', sampleJadeFilter);
+  jade.registerFilter('highlightcode', highlightCodeJadeFilter);
+  jade.registerFilter('ignorefrontmatter', ignoreFmJadeFilter);
+
+  // Configure marked
+  marked.setOptions({
+    renderer: markedRenderer,
+    langPrefix: '',
+    highlight(code, lang) {
+      return require('highlight.js').highlight(lang, code).value;
+    }
+  });
+
+  // initialize Metalsmith
+  let metalsmith = new Metalsmith(cwd);
+  metalsmith.source(metalsmithSource);
+
+  // filter all pages with draft set to true in front-matter
+  metalsmith.use(drafts());
+
+  // add metadata
+  metalsmith.use(define({
+      icons: fs.readdirSync(path.join(cwd, 'icons')).map(file => ({ name: file.replace(/\.svg$/, '') })),
+      colors: readJSONFile(path.join(cwd, 'less/colors.json')),
+      version: readJSONFile(path.join(cwd, 'tmp/version.json')),
+      package: readJSONFile(path.join(cwd, 'package.json')),
+      config,
+      moment,
+      marked,
+      changelogRenderer: markedChangelogRenderer,
+      // basedir for layout files
+      basedir: jadeBaseDir
+    })
+  );
+
+  // Do the markdown pages
+  metalsmith.use(
+    branch(['**/*.md', '!_*/**/*.md'])
+      .use(relative())
+      .use(markdown({
+          useMetadata: true,
+          marked
+        })
+
+      ));
+
+  // Do the jade pages
+  metalsmith.use(
+    branch(['**/*.jade'])
+      .use(filepath({
+        absolute: true})
+      )
+      .use(function(files, metalsmith, done) {
+        _.forEach(files, file => {
+            return file.link = `${file.link.slice(0, -5)}.html`;
+          }
+        );
+        return done();
+      })
+      .use(relative())
+      .use(collections({
+          navigation: {
+            sortBy: 'order',
+            refer: false
+          }
+        })
+      )
+      .use(jade({
+          useMetadata: true,
+          locals: metalsmith.metadata(),
+          pretty: true,
+          basedir: jadeBaseDir
+        })
+
+      ));
+
+  // Duplicate snippets to be used as demo pages (will be wrapped with templates below)
+  metalsmith.use(
+    branch(['**/*.html'])
+      .use(copy({
+          pattern: '**/snippets/*.html',
+          transform: file => file.replace(/snippets/i, 'demos')
+        })
+
+      ));
+
+  metalsmith.use(ignore('**/includes/**'));
+
+  // Add some pages to the search index
+  metalsmith.use(
+    branch([
+        '**/*.html',
+        '!**/snippets/*.html',
+        '!**/demos/*.html'
+      ]
+    )
+      .use(filepath({
+        absolute: true
+      }))
+      .use(relate())
+      .use(navigation())
+      .use(lunr({
+          includeAll: true,
+          fields: {
+            title: 10,
+            tags: 5,
+            contents: 1
+          }
+        })
+      )
+      .use(searchIndexData())
+  );
+
+  // Wrap the pages with their template
+  metalsmith.use(
+    branch([
+        '**/*.html',
+        '!**/snippets/*.html'
+      ]
+    )
+      .use(templates({
+          engine: 'jade',
+          directory: path.join(cwd, './docs/layouts')
+        })
+
+      ));
+
+  // Output
+  metalsmith.clean(false);
+  metalsmith.destination('./dist/docs/');
+
+  // Function to run the magic
+  let build = () =>
+      metalsmith.build(function(err) {
+        if (!err) { return cb(); } else { return cb(err); }
+      })
+    ;
+
+  let username = process.env.GITHUB_USERNAME;
+  let password = process.env.GITHUB_PASSWORD;
+
+  if (username && password) {
+    gutil.log(`Will load GitHub changelog with user ${username.cyan}`);
+
+    // Amazingly load our GitHub changelog to include it into our build!
+    return loadChangelog(username, password, function(err, changelog) {
+        if (!err) {
+          config.changelog = changelog;
+          gutil.log(`Successfully loaded ${`${changelog.length}`.cyan} changelog items!`);
+        } else {
+          gutil.log("Failed loading changelog items!".red);
+        }
+
+        return build();
+      }
+    );
+  } else {
+    // Just build it w/o changelog
+    return build();
+  }
+};
+
+//! Copyright AXA Versicherungen AG 2015
