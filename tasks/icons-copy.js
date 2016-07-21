f20641fa97e537c75182fb9b06128dbb8ca7782a
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:23:56 +0200

    fixed most eslint erros of gulp icons-copy task

--- tasks/icons-copy.js
+++ tasks/icons-copy.js
@@ -1,9 +1,8 @@
-import gulp from 'gulp';
+import gulp from 'gulp'
 
 export default () =>
 
-  gulp.src([ 'icons/**/*.svg' ])
+  gulp.src(['icons/**/*.svg'])
     .pipe(gulp.dest('./dist/icons/'))
-;
 
 //! Copyright AXA Versicherungen AG 2015
