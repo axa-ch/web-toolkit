d4cfeaac0227af5a77695761cffa33ab7368fa6e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:23:26 +0200

    fixed most eslint erros of gulp docs-styles task

--- tasks/docs-styles.js
+++ tasks/docs-styles.js
@@ -1,20 +1,19 @@
-import gulp from 'gulp';
+import gulp from 'gulp'
+import path from 'path'
 
-import path from 'path';
-
-import styles from '../lib/styles';
+import styles from '../lib/styles'
 
 export default () =>
 
-  styles([
-      './docs/less/docs.less'
+  styles(
+    [
+      './docs/less/docs.less',
     ],
     [
       path.join(__dirname, '../dist/less'),
-      path.join(__dirname, '../docs/less')
+      path.join(__dirname, '../docs/less'),
     ],
     './dist/docs/css'
   )
-;
 
 //! Copyright AXA Versicherungen AG 2015
