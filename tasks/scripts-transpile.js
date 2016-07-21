ba6c4881faac6732490da8cbb74953fa0cb93ac5
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:25:14 +0200

    fixed most eslint erros of gulp scripts-transpile task

--- tasks/scripts-transpile.js
+++ tasks/scripts-transpile.js
@@ -1,26 +1,27 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-import coffee from 'gulp-coffee';
-import merge from 'merge-stream';
-import sourcemaps from 'gulp-sourcemaps';
-import handleError from '../lib/handle-error';
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+import coffee from 'gulp-coffee'
+import merge from 'merge-stream'
+import sourcemaps from 'gulp-sourcemaps'
+
+import handleError from '../lib/handle-error'
 
 export default function() {
-  let coffey = gulp.src('jquery/*.coffee')
+  const coffey = gulp.src('jquery/*.coffee')
     .pipe(sourcemaps.init())
     .pipe(coffee({ bare: true }))
     .on('error', handleError('Coffee failed'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('js'))
 
-  let es6 = gulp.src('jquery/*.js')
+  const es6 = gulp.src('jquery/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
     .on('error', handleError('Babel failed'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('js'))
 
-  return merge(coffey, es6);
-};
+  return merge(coffey, es6)
+}
 
 //! Copyright AXA Versicherungen AG 2016
