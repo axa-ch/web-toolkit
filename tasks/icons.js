00159a268c8f7af892e1a46e65234ef03fc83709
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:24:08 +0200

    fixed most eslint erros of gulp icons task

--- tasks/icons.js
+++ tasks/icons.js
@@ -1,14 +1,13 @@
-import gulp from 'gulp';
-import svgmin from 'gulp-svgmin';
-import svgstore from 'gulp-svgstore';
+import gulp from 'gulp'
+import svgmin from 'gulp-svgmin'
+import svgstore from 'gulp-svgstore'
 
 export default () =>
 
-  gulp.src([ 'icons/**/*.svg' ])
+  gulp.src(['icons/**/*.svg'])
     .pipe(svgmin())
     .pipe(svgstore())
     .pipe(gulp.dest('./dist/images/'))
     .pipe(gulp.dest('./dist/docs/images/'))
-;
 
 //! Copyright AXA Versicherungen AG 2015
