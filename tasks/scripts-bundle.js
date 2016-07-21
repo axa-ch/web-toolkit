e302799d7837c205b702bec9a300bf6088fe0520
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:24:59 +0200

    fixed most eslint erros of gulp scripts-bundle task

--- tasks/scripts-bundle.js
+++ tasks/scripts-bundle.js
@@ -1,14 +1,15 @@
-import babelify from 'babelify';
-import browserify from 'browserify';
-import shim from 'browserify-shim';
-import coffeeify from 'coffeeify';
-import gulp from 'gulp';
-import sourcemaps from 'gulp-sourcemaps';
-import handleError from '../lib/handle-error';
-import buffer from 'vinyl-buffer';
-import source from 'vinyl-source-stream';
-import uglify from 'gulp-uglify';
-import rename from 'gulp-rename';
+import babelify from 'babelify'
+import browserify from 'browserify'
+import shim from 'browserify-shim'
+import coffeeify from 'coffeeify'
+import gulp from 'gulp'
+import sourcemaps from 'gulp-sourcemaps'
+import buffer from 'vinyl-buffer'
+import source from 'vinyl-source-stream'
+import uglify from 'gulp-uglify'
+import rename from 'gulp-rename'
+
+import handleError from '../lib/handle-error'
 
 export default () =>
   browserify()
@@ -26,6 +27,5 @@ export default () =>
     .pipe(rename({ extname: '.min.js' }))
     .pipe(sourcemaps.write(''))
     .pipe(gulp.dest('dist/jquery'))
-;
 
 //! Copyright AXA Versicherungen AG 2016
