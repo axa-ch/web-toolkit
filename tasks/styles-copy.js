8b19c23d4bf4c2d3322d56a25a7473c554128e85
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:26:16 +0200

    fixed most eslint erros of gulp styles-copy task

--- tasks/styles-copy.js
+++ tasks/styles-copy.js
@@ -1,9 +1,8 @@
-import gulp from 'gulp';
+import gulp from 'gulp'
 
 export default () =>
 
-  gulp.src([ './less/**/*.less' ], { base: './less' })
+  gulp.src(['./less/**/*.less'], { base: './less' })
     .pipe(gulp.dest('./dist/less'))
-;
 
 //! Copyright AXA Versicherungen AG 2015
