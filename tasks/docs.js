38f3f7936f5e7e86e10544adcb506892f89a6d52
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-21 19:23:37 +0200

    fixed most eslint erros of gulp docs task

--- tasks/docs.js
+++ tasks/docs.js
@@ -1,13 +1,11 @@
-import runSequence from 'run-sequence';
+import runSequence from 'run-sequence'
 
 export default cb =>
   runSequence([
-      'docs-pages',
-      'docs-scripts',
-      'docs-styles',
-      'docs-assets'
-    ],
-    cb)
-;
+    'docs-pages',
+    'docs-scripts',
+    'docs-styles',
+    'docs-assets',
+  ], cb)
 
 //! Copyright AXA Versicherungen AG 2015
