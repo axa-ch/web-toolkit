c4ab94615cc0dc3e81a2a9ed67b8462ccdff866b
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 13:31:52 +0200

    removed transpilation of coffee scripts

--- tasks/scripts-transpile.js
+++ tasks/scripts-transpile.js
@@ -1,27 +1,16 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
-import coffee from 'gulp-coffee'
-import merge from 'merge-stream'
 import sourcemaps from 'gulp-sourcemaps'
 
 import handleError from '../lib/handle-error'
 
 export default function () {
-  const coffey = gulp.src('jquery/*.coffee')
-    .pipe(sourcemaps.init())
-    .pipe(coffee({ bare: true }))
-    .on('error', handleError('Coffee failed'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('js'))
-
-  const es6 = gulp.src('jquery/*.js')
+  return gulp.src('jquery/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
     .on('error', handleError('Babel failed'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('js'))
-
-  return merge(coffey, es6)
 }
 
 //! Copyright AXA Versicherungen AG 2016
