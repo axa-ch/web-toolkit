1e6865d689478f75024619696a9258c1ad54cafa
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 10:41:16 +0200

    fixed gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,15 @@
-import gulp from 'gulp';
-import del from 'del';
-import runSequence from 'run-sequence';
-import readJSONFile from './lib/readJSONFile';
-import errorify from './lib/errorify';
-import file from './lib/file';
-import after from './lib/after';
-import config from './package.json';
+import gulp from 'gulp'
+import del from 'del'
+import runSequence from 'run-sequence'
+import gulpLoadTasks from 'gulp-load-tasks'
+import readJSONFile from './lib/readJSONFile'
+import errorify from './lib/errorify'
+import file from './lib/file'
+import after from './lib/after'
+import config from './package.json'
 
-require('gulp-load-tasks')({ extensions: [ '.coffee' ] });
+gulpLoadTasks()
 
-gulp.task('default', [ 'build' ]);
+gulp.task('default', ['build'])
 
 //! Copyright AXA Versicherungen AG 2015
