d9a8301478b5d2450845b374b786c48230731111
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 10:09:05 +0200

    fixed most eslint errors jquery modal

--- jquery/modal.js
+++ jquery/modal.js
@@ -1,54 +1,52 @@
-import $ from 'jquery';
+/* global document */
+
+import $ from 'jquery'
 
 class Modal {
 
-  constructor(element, options) {
-    this.$element = $(element);
+  constructor(element) {
+    this.$element = $(element)
   }
 
   toggle() {
-
     if (this.$element.hasClass('is-active')) {
-      this.$element.removeClass('is-active');
-      return $('body').removeClass('is-modal-open');
+      this.$element.removeClass('is-active')
+      $('body').removeClass('is-modal-open')
     } else {
-      this.$element.addClass('is-active');
-      return $('body').addClass('is-modal-open');
+      this.$element.addClass('is-active')
+      $('body').addClass('is-modal-open')
     }
   }
 }
 
 // Plugin definition
-let Plugin = function(option) {
-  let params = arguments;
+const Plugin = (option) => this.each(() => {
+  const $this = $(this)
+  let data = $this.data('axa.modal')
 
-  return this.each(function() {
-    let $this = $(this);
-    let data = $this.data('axa.modal');
+  if (!data) {
+    data = new Modal(this)
+    $this.data('axa.modal', data)
+  }
 
-    if (!data) {
-      data = new Modal(this);
-      $this.data('axa.modal', data);
-    }
+  if (typeof option === 'string') {
+    data[option]()
+  }
 
-    if (typeof option === 'string') {
-      return data[option]();
-    }
-  });
-};
+  return data
+})
 
 // Plugin registration
-$.fn.modal = Plugin;
-$.fn.modal.Constructor = Modal;
+$.fn.modal = Plugin
+$.fn.modal.Constructor = Modal
 
 // DATA-API
-$(document).on('click.axa.modal.data-api', '[data-modal]', function(e) {
-  e.preventDefault();
+$(document).on('click.axa.modal.data-api', '[data-modal]', (e) => {
+  e.preventDefault()
 
-  let $target = $($(e.currentTarget).data('modal'));
+  const $target = $($(e.currentTarget).data('modal'))
 
-  return Plugin.call($target, 'toggle');
-}
-);
+  Plugin.call($target, 'toggle')
+})
 
 //! Copyright AXA Versicherungen AG 2015
