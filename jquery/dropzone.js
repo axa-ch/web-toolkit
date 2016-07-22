37fd1d0eded6f0649694aa8ed83dbaad51e1188d
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 09:41:40 +0200

    fixed most eslint errors jquery dropzone

--- jquery/dropzone.js
+++ jquery/dropzone.js
@@ -1,63 +1,61 @@
-import $ from 'jquery';
+/* global window */
+
+import $ from 'jquery'
 
 // Public class definition
 class Dropzone {
 
   constructor(element, options) {
-    this.element = element;
-    this.$element = $(element);
-    this.options = $.extend({}, options);
+    this.element = element
+    this.$element = $(element)
+    this.options = {
+      ...options,
+    }
 
-    this.init();
+    this.init()
   }
 
   init() {
-    this.$element.bind('dragover', this, function(event) {
-      event.preventDefault();
-      return event.data.$element.addClass('dropzone__container--dragover');
-    }
-    );
+    this.$element.bind('dragover', this, (event) => {
+      event.preventDefault()
+      event.data.$element.addClass('dropzone__container--dragover')
+    })
 
-    this.$element.bind('dragleave', this, function(event) {
-      event.preventDefault();
-      return event.data.$element.removeClass('dropzone__container--dragover');
-    }
-    );
+    this.$element.bind('dragleave', this, (event) => {
+      event.preventDefault()
+      event.data.$element.removeClass('dropzone__container--dragover')
+    })
 
-    return this.$element.on('drop', this, function(event) {
-      event.preventDefault();
-      return event.data.$element.removeClass('dropzone__container--dragover');
-    }
-    );
+    return this.$element.on('drop', this, (event) => {
+      event.preventDefault()
+      event.data.$element.removeClass('dropzone__container--dragover')
+    })
   }
 }
 
 // Plugin definition
-let Plugin = function(option) {
-  let params = arguments;
+const Plugin = () => this.each(() => {
+  const $this = $(this)
+  let data = $this.data('axa.dropzone')
 
-  return this.each(function() {
-    let $this = $(this);
-    let data = $this.data('axa.dropzone');
+  if (!data) {
+    data = new Dropzone(this)
+    $this.data('axa.dropzone', data)
+  }
 
-    if (!data) {
-      data = new Dropzone(this);
-      return $this.data('axa.dropzone', data);
-    }
-  });
-};
+  return data
+})
 
 // Plugin registration
-$.fn.dropzone = Plugin;
-$.fn.dropzone.Constructor = Dropzone;
+$.fn.dropzone = Plugin
+$.fn.dropzone.Constructor = Dropzone
 
 // DATA-API
 $(window).on('load', () =>
-  $('[data-dropzone="dropzone"]').each(function() {
-    let $dropzone = $(this);
-    return Plugin.call($dropzone);
+  $('[data-dropzone="dropzone"]').each(() => {
+    const $dropzone = $(this)
+    Plugin.call($dropzone)
   })
-
-);
+)
 
 //! Copyright AXA Versicherungen AG 2015
