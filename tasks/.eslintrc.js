016d3661f06ee0a4d8dee744e7b1b82c06b8b30f
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 13:21:49 +0200

    fixing eslint no-extraneous-dependencies errors for devDependencies in gulps's task folder

---
+++ tasks/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "rules": {
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": false,
+      "optionalDependencies": false,
+      "peerDependencies": false
+    }]
+  }
+}
