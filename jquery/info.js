a9fdb2ae8a48ceb92219627af62b1223f3842efb
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 09:50:05 +0200

    fixed most eslint errors jquery info

--- jquery/info.js
+++ jquery/info.js
@@ -1,56 +1,52 @@
-import $ from 'jquery';
+/* global window */
+
+import $ from 'jquery'
 
 // Public class definition
 class Info {
-
   constructor(element, options) {
-    this.$element = $(element);
+    this.$element = $(element)
     // @options = $.extend {}, options
 
-    let selector = this.$element.data('target');
-    if (selector == null) { selector = options.target; }
+    let selector = this.$element.data('target')
+    if (selector == null) { selector = options.target }
 
-    this.$target = $(selector);
+    this.$target = $(selector)
 
     this.$element.on('click', this, event => event.data.toggle(event)
-    );
+    )
   }
 
   toggle() {
-    this.$target.slideToggle();
-    return this.$element.toggleClass('is-active');
+    this.$target.slideToggle()
+    return this.$element.toggleClass('is-active')
   }
 }
 
 // Plugin definition
-let Plugin = function(option) {
-  let params = arguments;
+const Plugin = (option) => this.each(() => {
+  const $this = $(this)
+  let data = $this.data('axa.info')
+  const options = $.extend({}, data, typeof option === 'object' && option)
 
-  return this.each(function() {
-    let options = $.extend({}, data, typeof option === 'object' && option);
-    if (typeof option === 'string') { let action = option; }
+  if (!data) {
+    data = new Info(this, options)
+    $this.data('axa.info', data)
+  }
 
-    let $this = $(this);
-    var data = $this.data('axa.info');
-
-    if (!data) {
-      data = new Info(this, options);
-      return $this.data('axa.info', data);
-    }
-  });
-};
+  return data
+})
 
 // Plugin registration
-$.fn.info = Plugin;
-$.fn.info.Constructor = Info;
+$.fn.info = Plugin
+$.fn.info.Constructor = Info
 
 // DATA-API
 $(window).on('load', () =>
-  $('[data-info]').each(function() {
-    let $info = $(this);
-    return Plugin.call($info);
+  $('[data-info]').each(() => {
+    const $info = $(this)
+    Plugin.call($info)
   })
-
-);
+)
 
 //! Copyright AXA Versicherungen AG 2015
