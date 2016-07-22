36be69cfcfeaf76774594d96ebe3fd3fb99eff1a
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 13:46:41 +0200

    pretified gulp styles task

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,5 +1,10 @@
 import runSequence from 'run-sequence'
 
-export default cb => runSequence('styles-copy', 'styles-variables', 'styles-compile', cb)
+export default cb =>
+  runSequence(
+    'styles-copy',
+    'styles-variables',
+    'styles-compile',
+    cb)
 
 //! Copyright AXA Versicherungen AG 2015
