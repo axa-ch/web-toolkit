8e49d99b86e040cc9dbe3743b3caf5c24d4dbe2e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 10:09:19 +0200

    fixed most eslint errors jquery notification

--- jquery/notification.js
+++ jquery/notification.js
@@ -1,121 +1,116 @@
-import $ from 'jquery';
+/* global document */
+
+import $ from 'jquery'
 
 class NotificationPane {
 
-  constructor(element, options) {
-    this.$element = $(element);
-    console.log(this.$element);
+  constructor(element) {
+    this.$element = $(element)
+
+    const $element = this.$element
+
     this.path = {
-      info: this.$element.data('notification-info'),
-      success: this.$element.data('notification-success'),
-      error: this.$element.data('notification-error')
-    };
+      info: $element.data('notification-info'),
+      success: $element.data('notification-success'),
+      error: $element.data('notification-error'),
+    }
+
+    // @path.info = @$element.data 'notification-info'
+    // @path.success = @$element.data 'notification-success'
+    // @path.error = @$element.data 'notification-error'
   }
-    //@path.info = @$element.data 'notification-info'
-    //@path.success = @$element.data 'notification-success'
-    //@path.error = @$element.data 'notification-error'
 
   displayNotification(notification) {
-
     if (notification == null) {
-      return;
+      return
     }
 
-    let $notification = $('<div class="notifications__item" ></div>');
-    let $icon = null;
+    const $notification = $('<div class="notifications__item" ></div>')
+    let $icon = null
 
     if (notification.modifier) {
-      console.log(notification.modifier);
-      console.log(this.path);
-      console.log(this.path[notification.modifier]);
-      $notification.addClass(`notifications__item--${notification.modifier}`);
-      $icon = `<svg class="icon notifications__item__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="${this.path[notification.modifier]}"></use></svg>`;
-      console.log($icon);
+      $notification.addClass(`notifications__item--${notification.modifier}`)
+      $icon = `<svg class="icon notifications__item__icon">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="${this.path[notification.modifier]}"></use>
+      </svg>`
     }
 
-    let $iconContainer = $('<div class="notifications__item__icon-container">');
+    const $iconContainer = $('<div class="notifications__item__icon-container">')
 
-    $iconContainer.append($icon);
-    $notification.append($iconContainer);
+    $iconContainer.append($icon)
+    $notification.append($iconContainer)
 
-    $notification.on('click', function() {
-      return this.hideNotification($notification);
-    }
-    );
+    $notification.on('click', () => {
+      this.hideNotification($notification)
+    })
 
-    let $content = $('<div class="notifications__item__content"></div>');
+    const $content = $('<div class="notifications__item__content"></div>')
+
     if (notification.html === true) {
-      $content.html(notification.content);
+      $content.html(notification.content)
     } else {
-      $content.text(notification.content);
+      $content.text(notification.content)
     }
 
-    $notification.append($content);
+    $notification.append($content)
 
-    let timeout = 2000;
+    let timeout = 2000
 
-    if (typeof notification.timeout === "number") {
-      ({ timeout } = notification);
+    if (typeof notification.timeout === 'number') {
+      ({ timeout } = notification)
     }
 
     setTimeout((() => {
-      return this.hideNotification($notification);
-    }
-    ), timeout);
+      this.hideNotification($notification)
+    }), timeout)
 
-    return this.$element.append($notification);
+    this.$element.append($notification)
   }
 
   hideNotification($notification) {
-    $notification.addClass('notifications__item--fade-out');
-    return setTimeout((() => $notification.remove()), 500);
+    $notification.addClass('notifications__item--fade-out')
+    setTimeout((() => $notification.remove()), 500)
   }
 }
 
 // Plugin definition
-let Plugin = function(option) {
-  let params = arguments;
+const Plugin = (option) => this.each(() => {
+  const $this = $(this)
+  let data = $this.data('axa.notification')
 
-  return this.each(function() {
-    let $this = $(this);
-    let data = $this.data('axa.notification');
+  if (!data) {
+    data = new NotificationPane(this)
+    $this.data('axa.notification', data)
+  }
 
-    if (!data) {
-      data = new NotificationPane(this);
-      $this.data('axa.notification', data);
-    }
+  if (typeof option === 'object') {
+    data.displayNotification(option)
+  }
 
-    if (typeof option === 'object') {
-      data.displayNotification(option);
-    }
+  if (typeof option === 'string') {
+    data.displayNotification({
+      content: option,
+    })
+  }
 
-    if (typeof option === 'string') {
-      return data.displayNotification({
-        content: option
-      }
-      );
-    }
-  });
-};
+  return data
+})
 
 // Plugin registration
-$.fn.notification = Plugin;
-$.fn.notification.Constructor = NotificationPane;
+$.fn.notification = Plugin
+$.fn.notification.Constructor = NotificationPane
 
 // DATA-API
-$(document).on('click.axa.notification.data-api', '[data-notification]', function(e) {
-  e.preventDefault();
+$(document).on('click.axa.notification.data-api', '[data-notification]', (e) => {
+  e.preventDefault()
 
-  let $this = $(this);
+  const $this = $(this)
+  const $target = $($this.data('notification'))
 
-  let $target = $($this.data('notification'));
-
-  return Plugin.call($target, {
+  Plugin.call($target, {
     content: $this.data('notification-content'),
-    modifier: $this.data('notification-modifier')
-  }
-  );
-}
-);
+    modifier: $this.data('notification-modifier'),
+  })
+})
 
 //! Copyright AXA Versicherungen AG 2015
