9f57341890dcad6d0d3f39acbf0acfe087bca530
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 09:47:15 +0200

    fixed most eslint errors jquery ie9-spinner

--- jquery/ie9-spinner.js
+++ jquery/ie9-spinner.js
@@ -1,62 +1,60 @@
-import $ from 'jquery';
+/* global window, document */
+
+import $ from 'jquery'
 
 // Public class definition
 class IE9Spinner {
+  constructor(element) {
+    this.$element = $(element)
 
-  constructor(element, options) {
-    this.$element = $(element);
-
-    this.$element.addClass('is-fallback-active');
+    this.$element.addClass('is-fallback-active')
   }
 }
 
 // Plugin definition
-let Plugin = function(option) {
-  let params = arguments;
+const Plugin = (option) => this.each(() => {
+  const $this = $(this)
+  let data = $this.data('axa.ie9Spinner')
+  const options = $.extend({}, data, typeof option === 'object' && option)
 
-  return this.each(function() {
-    let options = $.extend({}, data, typeof option === 'object' && option);
-    if (typeof option === 'string') { let action = option; }
+  if (!data) {
+    data = new IE9Spinner(this, options)
+    $this.data('axa.ie9Spinner', data)
+  }
 
-    let $this = $(this);
-    var data = $this.data('axa.ie9Spinner');
-
-    if (!data) {
-      data = new IE9Spinner(this, options);
-      return $this.data('axa.ie9Spinner', data);
-    }
-  });
-};
+  return data
+})
 
 // Plugin registration
-$.fn.ie9Spinner = Plugin;
-$.fn.ie9Spinner.Constructor = IE9Spinner;
+$.fn.ie9Spinner = Plugin
+$.fn.ie9Spinner.Constructor = IE9Spinner
 
 // DATA-API
-$(window).on('load', function() {
-  //check for support of the animation property
-  let elm = document.createElement('div');
-  let properties = [
+$(window).on('load', () => {
+  // check for support of the animation property
+  const elm = document.createElement('div')
+  const properties = [
     'animation',
     'WebkitAnimation',
     'MozAnimation',
     'msAnimation',
-    'OAnimation'
-  ];
+    'OAnimation',
+  ]
+
   for (let i = 0; i < properties.length; i++) {
-    let property = properties[i];
+    const property = properties[i]
+
+    // if the animation property is supported, exit
     if (elm.style[property] != null) {
-      //if the animation property is supported, exit
-      return;
+      return
     }
   }
 
-  //animation property not supported, activate fallback on all spinners
-  return $('[data-spinner]').each(function() {
-    let $spinner = $(this);
-    return Plugin.call($spinner);
-  });
-}
-);
+  // animation property not supported, activate fallback on all spinners
+  $('[data-spinner]').each(() => {
+    const $spinner = $(this)
+    Plugin.call($spinner)
+  })
+})
 
 //! Copyright AXA Versicherungen AG 2015
