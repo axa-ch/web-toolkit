f07ac0625f88eb47b0071760debea4d52d430868
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-07-22 09:57:41 +0200

    fixed most eslint errors jquery menu-collapsing

--- jquery/menu-collapsing.js
+++ jquery/menu-collapsing.js
@@ -1,85 +1,93 @@
-import $ from 'jquery';
+/* global window */
+
+import $ from 'jquery'
 
 // Public class definition
 class CollapsingMenu {
-  static DEFAULTS =
-    {exclusive: false};
+  static DEFAULTS = {
+    exclusive: false,
+  }
 
   constructor(element, options) {
-    this.$element = $(element);
-    this.options = $.extend({}, CollapsingMenu.DEFAULTS, options);
+    this.$element = $(element)
+    this.options = {
+      ...CollapsingMenu.DEFAULTS,
+      ...options,
+    }
 
-    this.init();
+    this.init()
   }
 
   init() {
-    return this.$element.on('click', '[data-link]', this, function(event) {
-      let link = $(event.target);
-      let subLevel = link.siblings('[data-level]');
+    this.$element.on('click', '[data-link]', this, (event) => {
+      const link = $(event.target)
+      const subLevel = link.siblings('[data-level]')
 
       if (subLevel.length > 0) {
-        event.preventDefault();
-        return event.data.toggle(subLevel);
+        event.preventDefault()
+        event.data.toggle(subLevel)
       }
-    }
-    );
+    })
   }
 
   toggle(toSet) {
-    let level = this.$element.find(toSet);
+    const level = this.$element.find(toSet)
 
-    if (!level) { throw new Error('Provided level not in menu!'); }
+    if (!level) { throw new Error('Provided level not in menu!') }
 
-    let parentLinks = level.parentsUntil(this.$element, '[data-link]');
-    let parentLevels = level.parentsUntil(this.$element, '[data-level]');
+    // const parentLinks = level.parentsUntil(this.$element, '[data-link]')
+    const parentLevels = level.parentsUntil(this.$element, '[data-level]')
 
-    let shouldOpen = !level.hasClass('is-open');
+    const shouldOpen = !level.hasClass('is-open')
 
     if (shouldOpen && this.options.exclusive) {
-      this.$element.find('[data-level]').not(parentLevels)
+      this.$element.find('[data-level]')
+        .not(parentLevels)
         .removeClass('is-open')
-        .siblings('[data-link]').removeClass('is-active');
+        .siblings('[data-link]')
+        .removeClass('is-active')
     }
 
-    return level.toggleClass('is-open', shouldOpen)
-      .siblings('[data-link]').toggleClass('is-active', shouldOpen);
+    level.toggleClass('is-open', shouldOpen)
+      .siblings('[data-link]')
+      .toggleClass('is-active', shouldOpen)
   }
 }
 
 // Plugin definition
-let Plugin = function(option) {
-  let params = arguments;
+const Plugin = (option) => {
+  const params = arguments
 
-  return this.each(function() {
-    let $this = $(this);
-    let options = $.extend({}, CollapsingMenu.DEFAULTS, data, typeof option === 'object' && option);
-    if (typeof option === 'string') { var action = option; }
-    var data = $this.data('axa.menu');
+  return this.each(() => {
+    const $this = $(this)
+    let data = $this.data('axa.menu')
+    const options = $.extend({}, CollapsingMenu.DEFAULTS, data, typeof option === 'object' && option)
 
     if (!data) {
-      data = new CollapsingMenu(this, options);
-      $this.data('axa.menu', data);
+      data = new CollapsingMenu(this, options)
+      $this.data('axa.menu', data)
     }
 
-    if (action === 'toggle') {
-      return data.toggle(params[1]);
+    if (typeof option === 'string' && option === 'toggle') {
+      data.toggle(params[1])
     }
-  });
-};
+
+    return data
+  })
+}
 
 // Plugin registration
-$.fn.collapsingMenu = Plugin;
-$.fn.collapsingMenu.Constructor = CollapsingMenu;
+$.fn.collapsingMenu = Plugin
+$.fn.collapsingMenu.Constructor = CollapsingMenu
 
 // DATA-API
 $(window).on('load', () =>
-  $('[data-menu="collapsing"]').each(function() {
-    let $menu = $(this);
-    let data = $menu.data();
+  $('[data-menu="collapsing"]').each(() => {
+    const $menu = $(this)
+    const data = $menu.data()
 
-    return Plugin.call($menu, data);
+    return Plugin.call($menu, data)
   })
-
-);
+)
 
 //! Copyright AXA Versicherungen AG 2015
