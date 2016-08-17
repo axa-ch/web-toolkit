1005e84e6a0ecaafb7fa400f4c6400b744c675d5
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-08-17 17:43:28 +0200

    added missing doclet

--- jquery/dasherize.js
+++ jquery/dasherize.js
@@ -1,3 +1,8 @@
+/**
+ * Dasherizes any given string
+ * @param {string} string - The string which should be dasherized.
+ * @returns {string} - Returns the dasherized string.
+ */
 const dasherize = (string) =>
   string.replace(/[A-Z]/g, (char, index) =>
     (index !== 0 ? '-' : '') + char.toLowerCase()
