0822445d27bf943f5f8581fb5fdd2090f78f3386
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-08-22 15:53:09 +0200

    encapsulated is css3 animation supported test

---
+++ jquery/is-animation-supported.js
@@ -0,0 +1,25 @@
+/* global document */
+
+export default function isAnimationSupported() {
+  // check for support of the animation property
+  const elm = document.createElement('div')
+  const properties = [
+    'animation',
+    'WebkitAnimation',
+    'MozAnimation',
+    'msAnimation',
+    'OAnimation',
+  ]
+  const length = properties.length
+
+  for (let i = 0; i < length; i++) {
+    const property = properties[i]
+
+    // if the animation property is supported, exit
+    if (elm.style[property] != null) {
+      return true
+    }
+  }
+
+  return false
+}
