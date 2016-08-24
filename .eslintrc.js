01ba9d9b14525463ccc725c275c9da81dd070ed9
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-08-24 17:30:22 +0200

    updated eslintrc file for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 module.exports = {
-  "extends": "airbnb-base",
+  "extends": "airbnb",
   "parser": "babel-eslint",
   "parserOptions": {
     "ecmaVersion": 6,
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
-      "classes": true
+      "classes": true,
+      "jsx": true
     }
   },
+  "plugins": [
+    "react",
+  ],
   "rules": {
     "dot-notation": "off",
     "max-len": ["warn", 140, 2],
     "no-cond-assign": ["error", "except-parens"],
     "no-else-return": "warn",
     "no-use-before-define": ["error", { "functions": false, "classes": false }],
+    "react/jsx-boolean-value": "warn",
+    "react/jsx-closing-bracket-location": ["error", "after-props"],
+    "react/prop-types": "warn",
     "semi": ["error", "never"],
   },
   "globals": {},
