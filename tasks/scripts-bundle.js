a0a1330f1d89cf1acd460b0df15ceb8070766620
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-08-29 12:38:10 +0200

    added multiple bundle by browserify

--- tasks/scripts-bundle.js
+++ tasks/scripts-bundle.js
@@ -6,24 +6,49 @@ import buffer from 'vinyl-buffer'
 import source from 'vinyl-source-stream'
 import uglify from 'gulp-uglify'
 import rename from 'gulp-rename'
+import es from 'event-stream'
 
 import handleError from '../lib/handle-error'
 
-module.exports = () =>
-  browserify()
-    .transform(shim)
-    .add('js/index.js')
-    .bundle()
-    .on('error', handleError('Browserify failed'))
-    .pipe(source('style-guide.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('dist/jquery'))
-    .pipe(uglify())
-    .on('error', handleError('Uglify failed'))
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('dist/jquery'))
+module.exports = () => {
+  // we define our input files, which we want to have
+  // bundled:
+  const files = [
+    'jquery/index.js',
+    'react/index.js',
+    'index.js',
+  ]
+
+  const tasks = files.map((file) =>
+    browserify()
+      .transform(shim)
+      .add(`dist/js/${file}`)
+      .bundle()
+      .on('error', handleError('Browserify failed'))
+      .pipe(source(getSourceName(file)))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write(''))
+      .pipe(gulp.dest('dist/js'))
+      .pipe(uglify())
+      .on('error', handleError('Uglify failed'))
+      .pipe(rename({ extname: '.min.js' }))
+      .pipe(sourcemaps.write(''))
+      .pipe(gulp.dest('dist/js'))
+  )
+
+  // create a merged stream
+  return es.merge.apply(null, tasks)
+}
+
+function getSourceName(file) {
+  switch (file) {
+    case 'index.js':
+      return 'axa-web-style-guide-all.js'
+
+    default:
+      return file.replace('/index', `/${file.split('/')[0]}.bundle`)
+  }
+}
 
 //! Copyright AXA Versicherungen AG 2016
