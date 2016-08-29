608888030453c6bb19ed5ac17cbde2dc34b5bae6
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-08-29 12:37:12 +0200

    updated transpile target

--- tasks/scripts-transpile.js
+++ tasks/scripts-transpile.js
@@ -4,11 +4,11 @@ import sourcemaps from 'gulp-sourcemaps'
 
 import handleError from '../lib/handle-error'
 
-module.exports = () => gulp.src('jquery/*.js')
+module.exports = () => gulp.src(['js/**/*.js'])
   .pipe(sourcemaps.init())
   .pipe(babel())
   .on('error', handleError('Babel failed'))
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('js'))
+  .pipe(gulp.dest('dist/js'))
 
 //! Copyright AXA Versicherungen AG 2016
