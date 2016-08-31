541e965ea3e69975044cd9431c58b16b20788953
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-08-31 09:48:37 +0200

    encapsulated icon markup

---
+++ js/jquery/icon.js
@@ -0,0 +1,5 @@
+const icon = (path) => `<svg class="icon notifications__item__icon">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="${path}"></use>
+      </svg>`
+
+export default icon
