f806fa8cdbd099bda4a8bc25c97bf5728299bcda
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-09-06 18:45:28 +0200

    updated script bundler

--- tasks/scripts-bundle.js
+++ tasks/scripts-bundle.js
@@ -1,56 +1,45 @@
-import browserify from 'browserify'
-import shim from 'browserify-shim'
 import gulp from 'gulp'
 import sourcemaps from 'gulp-sourcemaps'
-import buffer from 'vinyl-buffer'
-import source from 'vinyl-source-stream'
 import uglify from 'gulp-uglify'
 import rename from 'gulp-rename'
-import es from 'event-stream'
+import through2 from 'through2'
+import concat from 'concat-stream'
 
+import createBundler from '../lib/createBundler'
 import handleError from '../lib/handle-error'
 
-module.exports = () => {
-  // we define our input files, which we want to have
-  // bundled:
-  const files = [
-    'jquery/index.js',
-    'react/index.js',
-    'index.js',
-  ]
+const transformBundler = (opts) => through2.obj(function(file, encoding, next) {
+  const bundler = createBundler(file.path, opts)
 
-  const tasks = files.map((file) =>
-    browserify({
-      extensions: ['.js', '.jsx'],
-    })
-      .transform(shim)
-      .add(`dist/js/${file}`)
-      .bundle()
-      .on('error', handleError('Browserify failed'))
-      .pipe(source(getSourceName(file)))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write(''))
-      .pipe(gulp.dest('dist/js'))
-      .pipe(uglify())
-      .on('error', handleError('Uglify failed'))
-      .pipe(rename({ extname: '.min.js' }))
-      .pipe(sourcemaps.write(''))
-      .pipe(gulp.dest('dist/js'))
-  )
+  bundler.bundle()
+    .on('error', handleError('Browserify failed'))
+    .pipe(concat(data => {
+      file.contents = data
+      next(null, file)
+    }))
+})
 
-  // create a merged stream
-  return es.merge.apply(null, tasks)
-}
-
-function getSourceName(file) {
-  switch (file) {
-    case 'index.js':
-      return 'axa-web-style-guide-all.js'
-
-    default:
-      return file.replace('/index', `/${file.split('/')[0]}.bundle`)
-  }
-}
+module.exports = () =>
+  gulp.src([
+    'dist/js/jquery/index.js',
+    'dist/js/react/index.js',
+    'dist/js/index.js',
+  ], { base: 'dist/js', read: false })
+  .pipe(transformBundler())
+  .pipe(sourcemaps.init({ loadMaps: true }))
+  .pipe(sourcemaps.write(''))
+  .pipe(rename((path) => {
+    if (path.dirname === '.') {
+      path.basename = 'axa-web-style-guide-all.js'
+    } else {
+      path.basename = path.dirname + '.bundle'
+    }
+  }))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(uglify())
+  .on('error', handleError('Uglify failed'))
+  .pipe(rename({ extname: '.min.js' }))
+  .pipe(sourcemaps.write(''))
+  .pipe(gulp.dest('dist/js'))
 
 //! Copyright AXA Versicherungen AG 2016
