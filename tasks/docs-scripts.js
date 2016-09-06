0a9f011b67b0473b23556414e0216c84a6293623
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-09-06 18:45:40 +0200

    updated docs script bundler

--- tasks/docs-scripts.js
+++ tasks/docs-scripts.js
@@ -4,32 +4,40 @@ import shim from 'browserify-shim'
 import gulp from 'gulp'
 import sourcemaps from 'gulp-sourcemaps'
 import gutil from 'gulp-util'
-import buffer from 'vinyl-buffer'
-import source from 'vinyl-source-stream'
+import rename from 'gulp-rename'
+import uglify from 'gulp-uglify'
 import notifier from 'node-notifier'
+import through2 from 'through2'
+import concat from 'concat-stream'
+
+import createBundler from '../lib/createBundler'
+import handleError from '../lib/handle-error'
+
+const transformBundler = (opts) => through2.obj(function(file, encoding, next) {
+  const bundler = createBundler(file.path, opts)
+
+  bundler.transform(babelify)
+    .bundle()
+    .on('error', handleError('Browserify failed'))
+    .pipe(concat(data => {
+      file.contents = data
+      next(null, file)
+    }))
+})
 
 module.exports = () =>
-  browserify({
-    debug: true,
-    extensions: ['.js', '.jsx'],
-  })
-  .transform(babelify)
-  .transform(shim)
-  .add('docs/js/index.js')
-  .bundle()
-  .on('error', (err) => {
-    gutil.log(err.message)
-    notifier.notify({
-      title: 'Browserify Failed',
-      message: err.message,
-    })
-    return this.emit('end')
-  })
-  .pipe(source('style-guide-docs.js'))
-  .pipe(buffer())
-  .pipe(sourcemaps.init({ loadMaps: true }))
-  .on('error', gutil.log)
-  .pipe(sourcemaps.write(''))
-  .pipe(gulp.dest('dist/docs/js'))
+  gulp.src([
+    'docs/js/index.js',
+  ], { base: 'docs/js', read: false })
+    .pipe(transformBundler())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write(''))
+    .pipe(rename({ basename: 'style-guide-docs' }))
+    .pipe(gulp.dest('dist/docs/js'))
+    .pipe(uglify())
+    .on('error', handleError('Uglify failed'))
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('dist/docs/js'))
 
 //! Copyright AXA Versicherungen AG 2015
