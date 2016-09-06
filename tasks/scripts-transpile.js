465ab904ca0329288f90478bfe97dc5aa1f1a932
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-09-06 16:16:10 +0200

    optimized scripts-transpile

--- tasks/scripts-transpile.js
+++ tasks/scripts-transpile.js
@@ -1,14 +1,18 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import sourcemaps from 'gulp-sourcemaps'
+import remember from 'gulp-remember'
+import cached from 'gulp-cached'
 
 import handleError from '../lib/handle-error'
 
 module.exports = () => gulp.src(['js/**/*.js', 'js/**/*.jsx'])
+  .pipe(cached('scripts-transpile'))
   .pipe(sourcemaps.init())
   .pipe(babel())
   .on('error', handleError('Babel failed'))
   .pipe(sourcemaps.write())
+  .pipe(remember('scripts-transpile'))
   .pipe(gulp.dest('dist/js'))
 
 //! Copyright AXA Versicherungen AG 2016
