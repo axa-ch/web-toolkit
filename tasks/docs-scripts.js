98f840c594bf94399ad0761840efad9d1ca04c53
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-09-07 12:48:12 +0200

    cleaned up docs-scripts task

--- tasks/docs-scripts.js
+++ tasks/docs-scripts.js
@@ -1,34 +1,21 @@
-import babelify from 'babelify'
 import gulp from 'gulp'
 import sourcemaps from 'gulp-sourcemaps'
 import rename from 'gulp-rename'
-import through2 from 'through2'
-import concat from 'concat-stream'
+import babelify from 'babelify'
+import shim from 'browserify-shim'
 
 import noParse from '../lib/scriptsNoParse'
-import createBundler from '../lib/createBundler'
-import handleError from '../lib/handle-error'
-
-const transformBundler = (opts) => through2.obj(function (file, encoding, next) {
-  const bundler = createBundler(file.path, opts)
-
-  bundler.bundle()
-    .on('error', handleError('Browserify failed'))
-    .pipe(concat(data => {
-      file.contents = data
-      next(null, file)
-    }))
-})
+import browserify from '../lib/gulp-browserify'
 
 module.exports = () =>
   gulp.src([
     'docs/js/index.js',
   ], { base: 'docs/js', read: false })
-    .pipe(transformBundler({
+    .pipe(browserify({
       browserField: true,
       // bundleExternal: false,
       // detectGlobals: false,
-      transform: [babelify],
+      transform: [babelify, shim],
       noParse,
     }))
     .pipe(sourcemaps.init({ loadMaps: true }))
