ff1183f32606961652f01455b3fd7dcc0ab3871a
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-09-07 10:07:12 +0200

    utilized browserify options to improve docs-script build

--- tasks/docs-scripts.js
+++ tasks/docs-scripts.js
@@ -5,14 +5,14 @@ import rename from 'gulp-rename'
 import through2 from 'through2'
 import concat from 'concat-stream'
 
+import noParse from '../lib/scriptsNoParse'
 import createBundler from '../lib/createBundler'
 import handleError from '../lib/handle-error'
 
 const transformBundler = (opts) => through2.obj(function (file, encoding, next) {
   const bundler = createBundler(file.path, opts)
 
-  bundler.transform(babelify)
-    .bundle()
+  bundler.bundle()
     .on('error', handleError('Browserify failed'))
     .pipe(concat(data => {
       file.contents = data
@@ -24,7 +24,13 @@ module.exports = () =>
   gulp.src([
     'docs/js/index.js',
   ], { base: 'docs/js', read: false })
-    .pipe(transformBundler())
+    .pipe(transformBundler({
+      browserField: true,
+      bundleExternal: false,
+      detectGlobals: false,
+      transform: [babelify],
+      noParse,
+    }))
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write(''))
     .pipe(rename({ basename: 'style-guide-docs' }))
