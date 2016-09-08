367dcdc81013537eb3590062ffe9461a66872afe
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-09-08 15:18:03 +0200

    run webpack bundle task after browserify

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -3,6 +3,7 @@ import runSequence from 'run-sequence'
 module.exports = cb => runSequence(
   'scripts-transpile',
   'scripts-bundle',
+  'scripts-webpack',
   cb)
 
 //! Copyright AXA Versicherungen AG 2016
