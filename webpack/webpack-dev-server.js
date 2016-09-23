942e4dca864e3458d36f49804a796565833043f0
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-09-23 14:45:16 +0200

    utilized open on listen in webpack-dev-server

--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import webpack from 'webpack'
 import dev from 'webpack-dev-middleware'
 import hot from 'webpack-hot-middleware'
+import open from 'open'
 
 import createApp from '../lib/app'
 import webpackConfig from './bundle.dev.config.babel'
 
 const compiler = webpack(webpackConfig)
 
+const host = '0.0.0.0'
+const port = 3000
 const serverOptions = {
   contentBase: '/',
   quiet: true,
@@ -25,12 +28,14 @@ app.use(createApp())
 app.use(dev(compiler, serverOptions))
 app.use(hot(compiler))
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
   if (err) {
     // eslint-disable-next-line no-console
     console.error(err)
   } else {
     // eslint-disable-next-line no-console, max-len
-    console.info('🚧　Webpack development server listening on http://0.0.0.0:3000')
+    console.info(`    Webpack development server listening on http://${host}:${port}`)
+
+    open(`http://${host}:${port}`)
   }
 })
