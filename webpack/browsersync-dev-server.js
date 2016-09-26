2a5a0a9da3a0b38ad5e1600503805c2723a0ee62
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-09-26 09:46:44 +0200

    added browser sync dev server

---
+++ webpack/browsersync-dev-server.js
@@ -0,0 +1,52 @@
+/**
+ * Require Browsersync along with webpack and middleware for it
+ */
+import path from 'path'
+import browserSync from 'browser-sync'
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+
+/**
+ * Require ./webpack.config.js and make a bundler from it
+ */
+import webpackConfig from './bundle.dev.config.babel'
+
+const bundler = webpack(webpackConfig)
+const host = '0.0.0.0'
+const port = 3000
+
+/**
+ * Run Browsersync and use middleware for Hot Module Replacement
+ */
+browserSync({
+  server: {
+    baseDir: [path.resolve(__dirname, '..', 'dist', 'docs')],
+
+    middleware: [
+      webpackDevMiddleware(bundler, {
+        // IMPORTANT: dev middleware can't access config, so we should
+        // provide publicPath by ourselves
+        publicPath: webpackConfig.output.publicPath,
+
+        // pretty colored output
+        stats: { colors: true },
+
+        // for other settings see
+        // http://webpack.github.io/docs/webpack-dev-middleware.html
+      }),
+
+      // bundler should be the same as above
+      webpackHotMiddleware(bundler),
+    ],
+  },
+
+  host,
+  port,
+
+  // no need to watch '*.js' here, webpack will take care of it for us,
+  // including full page reloads if HMR won't work
+  files: [
+    'app/*.html',
+  ],
+})
