02ccac7a3ea464bb3052808a84160036942b22b5
Gabriel Koch <koch_gabriel@hotmail.com>
2016-10-24 18:32:30 +0200

    Output diff image after comparing screenshots

--- tests/compare.js
+++ tests/compare.js
@@ -1,14 +1,13 @@
-var resemble = require('resemblejs')
+var resemble = require('node-resemble');
+var fs = require('fs');
 
 var diff = resemble('screenshots/components/demos/badge_375x200.png')
-  .compareTo('tests/expectations/components/demos/badge_375x200.png').onComplete(function(data){
+  .compareTo('expectations/components/demos/badge_375x200.png').onComplete(function(data){
     console.log(data);
-    /*
-    {
-      misMatchPercentage : 100, // %
-      isSameDimensions: true, // or false
-      dimensionDifference: { width: 0, height: -1 }, // defined if dimensions are not the same
-      getImageDataUrl: function(){}
-    }
-    */
+    var imgDataUrl = data.getImageDataUrl();
+    var base64Data = imgDataUrl.replace(/^data:image\/png;base64,/, "");
+
+    require("fs").writeFile("out.png", base64Data, 'base64', function(err) {
+      console.log(err);
+    });
 });
