e141d6376cc091b598cb66e6cb00d69fd9fe9655
FKSI <francois.ky@axa-groupsolutions.com>
2017-01-06 17:48:39 +0100

    Improve mobile footer
    Add arrow icons
    Back-button

--- js/jquery/footer.js
+++ js/jquery/footer.js
@@ -11,11 +11,20 @@ class Footer {
 
   toggle() {
     if (this.$element.hasClass('is-active')) {
-      $('.footer-social-container').show()
+      $('.footer-back').remove()
       this.$element.removeClass('is-active')
+      $('.footer-social-container').show()
       this.$parents.show()
     } else {
       $('.footer-social-container').hide()
+      this.$element.prepend(`<li
+        class="footer-back footer-link-item hidden-sm-up"
+        >
+          <a class="footer-link " href="#" data-footer="${this.$element.selector}">
+          <svg class="footer-back-icon" viewBox="0 0 24 24">
+	           <path fill-rule="evenodd" clip-rule="evenodd" d="M5.39 1.478L6.86-.025l10.3 10.522.004-.004 1.467 1.497-.003.005.004.004-1.472 1.503-.004-.005L6.9 23.975 5.434 22.48 15.688 12" data-reactid="37"></path>
+          </svg>
+        </a></li>`)
       this.$element.addClass('is-active')
       this.$parents.hide()
     }
